794d51ba3b6039a412de7e27fd83bc84f89d58c0
gzsr <95002561+herniqeu@users.noreply.github.com>
2022-05-27 19:00:37 -0300

    Update haskell_08.hs

--- haskell/haskell_08.hs
+++ haskell/haskell_08.hs
@@ -1,1 +1,26 @@
+taili [] = []
+taili (x:xs) = xs 
 
+lenghtq [] = 0
+lenghtq (x:xs) = 1 + lenghtq(xs)
+
+concatq [] ys = ys 
+concatq (x:xs) ys = x:concatq xs ys
+
+mapa fn [] = []
+mapa fn (x:xs) = concatq (fn x) (mapa fn xs)
+
+reverseu [] = []
+reverseu (x:xs) = concatq (reverseu xs) x
+
+headu [] = []
+headu (x:xs) = x
+
+tailu [] = []
+tailu (x:xs) = xs 
+
+init [] = []
+init (x:xs) = reverseu (tailu (reverse(x:xs)))
+
+list_drop n [] = []
+list_drop n (x:xs) = x:concatq (list_drop (n-1) xs)
