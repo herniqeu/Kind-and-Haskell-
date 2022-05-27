818191cc769fb2001c09b481e52ca4209eff7a51
gzsr <95002561+herniqeu@users.noreply.github.com>
2022-05-27 17:53:27 -0300

    Update haskell_02.hs

--- haskell/haskell_02.hs
+++ haskell/haskell_02.hs
@@ -1,1 +1,15 @@
+primeiro (x,y) = x
 
+segundo (x,y) = y
+
+eql_elements (x,y) = x == y
+
+eql_pairs ((x,y),(z,w)) = x == z && y == w
+
+eql_pairs2 ((x,y),(z,w)) = x /= z && z /= w
+
+create x y = (x,y)
+
+neg (x,y) = (not(x),not(y))
+
+swap (x,y) = (y,x)
