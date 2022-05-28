97a1581afcff6bb544e231be5b7e93e67fa0baa3
gzsr <95002561+herniqeu@users.noreply.github.com>
2022-05-27 21:50:20 -0300

    Update haskell_05.hs

--- haskell/haskell_05.hs
+++ haskell/haskell_05.hs
@@ -1,7 +1,13 @@
+// Basic Exercises involving Pairs
+
+// Swaps the elements of a pair
 swap (a,b) = (b,a)
 
+// Returns the first element of a pair
 first (a,b) = a
 
+// Returns the second element of a pair
 second (a,b) = b
 
+// Applies a function to both elements of a Pair
 mapa fn (a,b) = (fn(a), fn(b)) 
